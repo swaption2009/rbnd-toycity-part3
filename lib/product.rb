f6f9a42b18e74f6d09619f6e5e3e3efb41562ec8
swaption2009 <swaption2009@gmail.com>
2016-06-18 23:40:10 -0700

    add duplicate product error

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,25 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
   end
+
+  private
+
+  def add_to_products
+    unless @@products.include?(@title)
+      @@products << @title
+    else
+      raise DuplicateProductError, "#{@title} is a duplicate and can't be added."
+    end
+  end
+
 end
